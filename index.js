32128ee81358b81695a785a8857c3eaf3bc11a09
Avel7884 <Avel7884@users.noreply.github.com>
2017-11-02 01:25:04 +0500

    docker added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const multer = require('multer');
 const cors = require('cors')
 const emotionRecognizer = require('./emotionRecognizer')
 const path = require('path')
+const cmd = require('node-cmd')
 
 const app = express()
 const upload = multer()
@@ -14,23 +15,34 @@ app.use(cors())
 const directoryUpload = 'uploads'
 
 app.post('/detect', upload.array(), function(req, res) {
-  const base64Data = req.body.image.replace(/^data:image\/jpeg;base64,/, '')
-  const filePath = path.join(__dirname, directoryUpload, 'out.jpg')
+const base64Data = req.body.image.replace(/^data:image\/jpeg;base64,/, '')
+const filePath = path.join(__dirname, directoryUpload, 'out.jpg')
 
-  if (!fs.existsSync(directoryUpload)){
-    fs.mkdirSync(directoryUpload)
+if (!fs.existsSync(directoryUpload)){
+fs.mkdirSync(directoryUpload)
+}
+
+fs.writeFile(filePath, base64Data, 'base64', (err) => {
+if (err) console.log(err);
+
+const callback = (err, data, stderr) => {
+  // console.log('callback', err, data, stderr)
+if (err) return
+
+const emotions = emotionRecognizer(filePath)
+
+emotions.then(emotions => {
+  if (emotions[0]) {
+    emotions[0].reptilian = data
   }
+res.send(emotions)
+})
 
-  fs.writeFile(filePath, base64Data, 'base64', function(err) {
-    if (err) console.log(err);
+}
 
-    const emotions = emotionRecognizer(filePath)
-
-    emotions.then(emotions => {
-      console.log('emotions', emotions)
-      res.send(emotions)
-    })
-  })
+const command = 'recognize.bat'
+cmd.get(command, callback)
+})
 })
 
 app.use('/', express.static(__dirname + '/front/build'))
